46cb6d6da3553a6a8f71221672c24e46888bd495
Lyle Underwood <lyleunderwood@gmail.com>
2017-12-03 21:48:41 +0000

    added workaround case

---
+++ caseD.js
@@ -0,0 +1,39 @@
+// @flow
+import type { Rect as ExternalRect } from './external-rect';
+
+export type Rect = {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+};
+
+export type UsefulRect = ExternalRect & {
+  factor: number,
+};
+
+export type HackRect = UsefulRect & Rect;
+
+function useRect(rect: {
+  idx: number,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+}) {
+  console.log(rect.width);
+}
+
+const rect: HackRect = {
+  x: 0,
+  y: 0,
+  width: 100,
+  height: 100,
+  factor: 2,
+};
+
+useRect({
+  ...rect,
+  idx: 1,
+});
+
